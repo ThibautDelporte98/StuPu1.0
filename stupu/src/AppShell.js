df3a95e40448a424945a963a8d689e93be2eb0fc
ThibautDelporte98 <thibaut.delporte@student.howest.be>
2024-11-24 14:28:02 +0100

    react route fix

--- stupu/src/AppShell.js
+++ stupu/src/AppShell.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AppShell = ({ children }) => {
   return (
     <>
-        <div className="px-4 sm:px-8 py-2 bg-gray-100">
+        <div className="px-4 sm:px-8 py-2">
           {children}
         </div>
     </>
